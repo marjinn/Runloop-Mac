ce4d185538a4c5ee795dd6e3dceb13fb2c9239e7
mar Jinn <marjinn@mars-Mac.local>
2014-07-21 03:59:57 +0530

    trial

--- RunLooperWithPortMessaging/NSThreadAndNSPort.m
+++ RunLooperWithPortMessaging/NSThreadAndNSPort.m
@@ -22,17 +22,32 @@
 @implementation NSThreadAndNSPort
 
 -(NSThread*)LaunchAThread:(NSString*)threadName
+             WithSelector:(SEL)threadEntryPoint
+                   object:(id)obj
 {
     NSThread* thelaunchedThread = nil;
     
     thelaunchedThread =
     [[NSThread alloc]initWithTarget:(id)self
-                           selector:@selector(ThreadsEntryPoint:)
-                             object:nil];
+                           selector:threadEntryPoint
+                             object:obj];
     
     [thelaunchedThread setName:threadName];
     
     return thelaunchedThread;
+}
+
+-(void)startTheThread:(NSString*)threadName
+         WithSelector:(SEL)threadEntryPoint
+{
+    NSThread* thread = nil;
+    thread = [self LaunchAThread:threadName
+                    WithSelector:@selector(ThreadsEntryPoint:)
+                          object:nil];
+    if (thread)
+    {
+        [thread start];
+    }
 }
 
 -(void)ThreadsEntryPoint:(id)data
@@ -46,6 +61,7 @@
         
         NSRunLoop* thisThreadsRunLoop = nil;
         thisThreadsRunLoop = [NSRunLoop currentRunLoop];
+        thisThreadsRunLoop = nil;
         
         [self setThisThreadsRunLoop:thisThreadsRunLoop];
         
@@ -57,24 +73,46 @@
         [threadDict setValue:(id)[NSNumber numberWithBool:exitNow]
                       forKey:@"ThreadShouldExitNB"];
         
-        
+        //Input Source
         NSMessagePort* threadLocalPort = nil;
         threadLocalPort = [NSMessagePort new];
         
+        //retain local port
         [self setThisThreadsLocalPort:(NSPort *)threadLocalPort];
+        threadLocalPort = nil;
         
-        [threadLocalPort setDelegate:(id<NSPortDelegate>)self];
+        //Input Source handler function will be the NSPortDelegate function
+        [[self thisThreadsLocalPort] setDelegate:(id<NSPortDelegate>)self];
         
+        //add input source to runLoop
         [[self thisThreadsRunLoop] addPort:(NSPort *)threadLocalPort
                  forMode:NSDefaultRunLoopMode];
         
+        //name local port
         NSString* localPortName = nil;
         localPortName = @"thisThreadsLocalPort";
         
+        //register the NSMessagePort (local port) with the NSMessagePortNameServer
         [[NSMessagePortNameServer sharedInstance] registerPort:
          [self thisThreadsLocalPort]
                                                           name:localPortName];
         
+        while (moreWorkToDo && !exitNow)
+        {
+            // Do one chunk of a larger body of work here.
+            // Change the value of the moreWorkToDo Boolean when done.
+            // Run the run loop but timeout immediately if the input source isn't
+            // waiting to fire.
+            
+            //Any Time Consuming Work Can be Done Here
+        //eg: URL Operations
+            
+            [[self thisThreadsRunLoop] runUntilDate:[NSDate date]];
+            
+            // Check to see if an input source handler changed the exitNow value.
+            exitNow =
+            [[threadDict valueForKey:@"ThreadShouldExitNow"] boolValue];
+        }
         
         
         NS_HANDLER
@@ -90,6 +128,8 @@
 #pragma mark -
 - (void)handlePortMessage:(NSPortMessage *)message
 {
+    [[[self threadLaunchedByThisClass]threadDictionary] setValue:(id)[NSNumber numberWithBool:YES]
+                                                          forKey:@"ThreadShouldExitNow"];
     return;
 }
 
